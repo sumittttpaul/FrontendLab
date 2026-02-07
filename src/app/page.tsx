6f2f3c8fc58bc5970b580ad6cfa757032b48d9c4
sumittttpaul <sumitpaul.project@gmail.com>
2026-02-07 18:02:33 +0530

    feat: added box container for post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,37 @@
 import { CodePreview } from "@/designs/components/code-preview";
 import { VideoPlayer } from "@/designs/components/video-player";
 
+import { cn } from "@/libs/cn";
+
+const Crosshair = ({ className = "" }: { className?: string }) => (
+  <div className={cn("absolute z-20 flex size-6 max-h-6 min-h-6 max-w-6 min-w-6 items-center justify-center", className)}>
+    <div className="dark:bg-muted-foreground h-full w-px bg-black"></div>
+    <div className="dark:bg-muted-foreground absolute h-px w-full bg-black"></div>
+  </div>
+);
+
+const DashedLine = () => <span className="relative h-full border-r border-dashed border-black/12.5 dark:border-white/12.5" />;
+
 export default function Home() {
   return (
-    <main className="mx-auto flex size-full max-w-375 flex-col gap-4 overflow-x-hidden p-4">
-      <span className="select-none">Code viewer</span>
-      <div className="flex w-full gap-4">
-        <div className="flex w-full">
-          <CodePreview>Code Preview</CodePreview>
+    <main className="mx-auto size-full max-w-300 p-4 md:p-8">
+      <div className="relative mt-20 flex size-full flex-col items-center justify-center">
+        <div className="pointer-events-none absolute inset-0 z-0">
+          <div className="relative size-full border border-black/15 dark:border-white/15">
+            <div className="grid size-full grid-cols-3">
+              <DashedLine />
+              <DashedLine />
+              <Crosshair className="-top-[0.8rem] -left-[0.8rem]" />
+              <Crosshair className="-right-[0.8rem] -bottom-[0.8rem]" />
+            </div>
+          </div>
+        </div>
+        <div className="relative z-10 flex size-full flex-col gap-6 px-40 pt-20 pb-40">
+          Main content will here.
+          <VideoPlayer />
+          <CodePreview />
         </div>
       </div>
-      <span className="select-none">Video player</span>
-      <VideoPlayer />
     </main>
   );
 }
--- src/designs/components/code-preview.tsx
+++ src/designs/components/code-preview.tsx
@@ -6,49 +6,50 @@ import { HugeiconsIcon } from "@hugeicons/react";
 
 export function CodePreview({ children }: React.PropsWithChildren) {
   return (
-    <div className="relative w-full max-w-[calc(100vw-2rem)] overflow-hidden rounded-lg bg-black/2.5 bg-[radial-gradient(var(--pattern-fg)_1px,transparent_0)] bg-size-[10px_10px] bg-fixed pt-8 pl-8 [--pattern-fg:var(--color-black)]/5 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:inset-ring after:inset-ring-black/5 max-sm:pt-4 max-sm:pl-4 dark:bg-white/2.5 dark:[--pattern-fg:var(--color-white)]/10 dark:after:inset-ring-white/10">
-      <div className="outline-l-none size-full rounded-tl-xl bg-[oklch(0.145_0_0)] pt-1 pl-2 outline outline-[color-mix(in_oklab,var(--color-white)_10%,transparent)]">
-        <div className="flex gap-2 p-2">
-          <span className="size-3 rounded-full bg-white/20"></span>
-          <span className="size-3 rounded-full bg-white/20"></span>
-          <span className="size-3 rounded-full bg-white/20"></span>
-        </div>
-        <div className="flex size-full overflow-hidden rounded-tl-lg bg-[oklch(0.2_0_0)] outline outline-[color-mix(in_oklab,var(--color-white)_10%,transparent)]">
-          <div className="flex w-full flex-col">
-            <ScrollArea orientation="horizontal" className="relative w-full">
-              <div className="flex w-full justify-between gap-2.5">
-                <div className="flex items-center gap-2.5">
-                  <span className="flex items-center gap-2 border-t border-r border-t-[oklch(0.985_0_0)] border-r-[color-mix(in_oklab,var(--color-white)_10%,transparent)]/75 px-2 py-2.5 text-sm text-[oklch(0.985_0_0)] select-none">
-                    <HugeiconsIcon icon={ReactIcon} size={18} strokeWidth={1.5} className="text-[oklch(0.985_0_0)]" />
-                    palindrome.tsx
-                    <HugeiconsIcon icon={Cancel01Icon} size={16} strokeWidth={2} className="text-[oklch(0.985_0_0)] opacity-50" />
-                  </span>
-                  <HugeiconsIcon icon={Add01Icon} size={18} strokeWidth={2} className="text-[oklch(0.985_0_0)] opacity-50" />
+    <div className="bg-background relative flex w-full max-w-[calc(100vw-2rem)]">
+      <div className="relative w-full overflow-hidden rounded-lg bg-black/4 bg-[radial-gradient(var(--pattern-fg)_1px,transparent_0)] bg-size-[10px_10px] bg-fixed pt-8 pl-8 [--pattern-fg:var(--color-black)]/5 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:inset-ring after:inset-ring-black/5 max-sm:pt-4 max-sm:pl-4 dark:bg-white/4 dark:[--pattern-fg:var(--color-white)]/10 dark:after:inset-ring-white/10">
+        <div className="outline-l-none size-full rounded-tl-xl bg-[oklch(0_0_0)] pt-1 pl-2 outline outline-[color-mix(in_oklab,var(--color-white)_10%,transparent)]">
+          <div className="flex gap-2 p-2">
+            <span className="size-3 rounded-full bg-white/20"></span>
+            <span className="size-3 rounded-full bg-white/20"></span>
+            <span className="size-3 rounded-full bg-white/20"></span>
+          </div>
+          <div className="flex size-full overflow-hidden rounded-tl-lg bg-[oklch(0.18_0_0)] outline outline-[color-mix(in_oklab,var(--color-white)_10%,transparent)]">
+            <div className="flex w-full flex-col">
+              <ScrollArea orientation="horizontal" className="relative w-full">
+                <div className="flex w-full justify-between gap-2.5">
+                  <div className="flex items-center gap-2.5">
+                    <span className="flex items-center gap-2 border-t border-r border-t-[oklch(0.985_0_0)] border-r-[color-mix(in_oklab,var(--color-white)_10%,transparent)]/75 px-2 py-2.5 text-sm text-[oklch(0.985_0_0)] select-none">
+                      <HugeiconsIcon icon={ReactIcon} size={18} strokeWidth={1.5} className="text-[oklch(0.985_0_0)]" />
+                      palindrome.tsx
+                      <HugeiconsIcon icon={Cancel01Icon} size={16} strokeWidth={2} className="text-[oklch(0.985_0_0)] opacity-50" />
+                    </span>
+                    <HugeiconsIcon icon={Add01Icon} size={18} strokeWidth={2} className="text-[oklch(0.985_0_0)] opacity-50" />
+                  </div>
+                  <div className="flex items-center pr-1.5">
+                    <CopyContent content={children as string} variant="secondary" size="text" />
+                  </div>
                 </div>
-                <div className="flex items-center pr-1.5">
-                  <CopyContent content={children as string} variant="secondary" size="text" />
+              </ScrollArea>
+              <ScrollArea orientation="horizontal" className="relative w-full">
+                <div className="flex w-auto items-center gap-2 border-y border-y-[color-mix(in_oklab,var(--color-white)_10%,transparent)]/75 px-2 py-2.5 text-xs font-medium text-[oklch(0.708_0_0)] select-none">
+                  <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
+                  <span>frontendlab</span>
+                  <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
+                  <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
+                  <span>src</span>
+                  <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
+                  <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
+                  <span>app</span>
+                  <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
+                  <HugeiconsIcon icon={ReactIcon} size={14} strokeWidth={2} className="min-w-3.5" />
+                  <span>palindrome.tsx</span>
                 </div>
-              </div>
-            </ScrollArea>
-            <ScrollArea orientation="horizontal" className="relative w-full">
-              <div className="flex w-auto items-center gap-2 border-y border-y-[color-mix(in_oklab,var(--color-white)_10%,transparent)]/75 px-2 py-2.5 text-xs font-medium text-[oklch(0.708_0_0)] select-none">
-                <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
-                <span>frontendlab</span>
-                <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
-                <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
-                <span>src</span>
-                <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
-                <HugeiconsIcon icon={Folder01Icon} size={14} strokeWidth={2} className="min-w-3.5" />
-                <span>app</span>
-                <HugeiconsIcon icon={ArrowRight01Icon} size={14} strokeWidth={2} className="min-w-3.5 opacity-75" />
-                <HugeiconsIcon icon={ReactIcon} size={14} strokeWidth={2} className="min-w-3.5" />
-                <span>palindrome.tsx</span>
-              </div>
-            </ScrollArea>
-            <ScrollArea orientation="horizontal" className="relative w-full">
-              <div className="p-5 max-sm:p-3">
-                <SyntaxHighlighter
-                  code={`import React from "react";
+              </ScrollArea>
+              <ScrollArea orientation="horizontal" className="relative w-full">
+                <div className="p-5 max-sm:p-3">
+                  <SyntaxHighlighter
+                    code={`import React from "react";
 import uniquePropHOC from "./lib/unique-prop-hoc";
 
 // this comment is here to demonstrate an extremely long line length.
@@ -71,9 +72,10 @@ class Expire extends React.Component {
 }
 
 export default uniquePropHOC(["time", "seconds"])(Expire);`}
-                />
-              </div>
-            </ScrollArea>
+                  />
+                </div>
+              </ScrollArea>
+            </div>
           </div>
         </div>
       </div>
