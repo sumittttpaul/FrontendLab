7db044b52fcf2246e433468532ee71998e1c4776
sumittttpaul <sumitpaul.project@gmail.com>
2026-02-08 23:54:07 +0530

    chore: disabled home page for now.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { slugify } from "@/libs/helpers";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redirect("https://www.linkedin.com/in/sumitttpaul");
-  return <p>{slugify("How to build agents with filesystems and bash")}</p>;
+  redirect("https://www.linkedin.com/in/sumitttpaul");
 }
